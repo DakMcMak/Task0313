eae3967a48de2f0dc7454e8c217e852aa75cb5ed
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-19 23:40:43 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    String p1 = "Мама", p2 = "Мыла", p3 = "Раму";
+    System.out.println(p1 + p2 + p3);
+    System.out.println(p2 + p1 + p3);
+    System.out.println(p3 + p1 + p2);
+    System.out.println(p3 + p2 + p1);
+    System.out.println(p2 + p3 + p1);
+    System.out.println(p1 + p3 + p2);
+}
+}
